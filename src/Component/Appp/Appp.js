8f4667ed83afda552e81e13502bf384b2ca78092
ajibarebabajide <ajibarebabajide1@gmail.com>
2024-10-01 18:40:18 +0100

    content udpate

--- src/Component/Appp/Appp.js
+++ src/Component/Appp/Appp.js
@@ -39,17 +39,30 @@ const PerksSection = () => {
           <h3>Why choose us  still...</h3>
           
           {/* Web3 Tokens */}
-          <div className="item">
+          {/* <div className="item">
             <button className="toggle-btn first_btn" onClick={() => toggleSection('web3')}>
               Lighting fast results <span>{expanded.web3 ? '-' : '+'}</span>
             </button>
             {expanded.web3 && (
               <p>Explore the home, connect with landlords, and experience the future of renting. No middleman, no hassle. With Zillstate, your next home is just a clicks away</p>
             )}
+          </div> */}
+          <div className="item">
+            <button className="toggle-btn first_btn">
+              Lighting fast results <span>{expanded.web3 ? '-' : '+'}</span>
+            </button>
           </div>
           
           {/* Rent with agents wahala */}
-          <div className="item">
+          {/* <div className="item second">
+            <button className="toggle-btn second_btn" onClick={() => toggleSection('rent')}>
+              Bypass the middleman <span>{expanded.rent ? '-' : '+'}</span>
+            </button>
+            {expanded.rent && (
+              <p>Explore the home, connect with landlords, and experience the future of renting. No middleman, no hassle. With Zillstate, your next home is just a clicks away</p>
+            )}
+          </div> */}
+          <div className="item second">
             <button className="toggle-btn second_btn" onClick={() => toggleSection('rent')}>
               Bypass the middleman <span>{expanded.rent ? '-' : '+'}</span>
             </button>
@@ -60,13 +73,18 @@ const PerksSection = () => {
           
           {/* Win wild tokens */}
           <div className="item">
+            <button className="toggle-btn last_btn">
+              Pay with your sol token<span>{expanded.win ? '-' : '+'}</span>
+            </button>
+          </div>
+          {/* <div className="item">
             <button className="toggle-btn last_btn" onClick={() => toggleSection('win')}>
               Pay with your sol token<span>{expanded.win ? '-' : '+'}</span>
             </button>
             {expanded.win && (
               <p>Explore the home, connect with landlords, and experience the future of renting. No middleman, no hassle. With Zillstate, your next home is just a clicks away</p>
             )}
-          </div>
+          </div> */}
         </div>
       </div>
 
